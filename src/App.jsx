1c7de408c193e261cfc2a8d685ef87827c4b0ff5
Frank Gu <gushaopengfrank@gmail.com>
2025-02-17 22:55:43 -0600

    recipe added back

--- src/App.jsx
+++ src/App.jsx
@@ -20,25 +20,6 @@ const App = () => {
         <Route path="/home" element={<HomeScreen />} />
       </Routes>
     </Router>
-
-
-
-
-
-
-
-    // <div className="App">
-    //   <SignIn />
-    //   <HomeScreen />
-    //   <CameraComponent />
-    //   <Recipe 
-    //   title="Samosa's"
-    //   imageURL="https://picsum.photos/200"
-    //   description="Great Indian appetizer"
-    //   ingredients="white potatoes, salt to taste, red pepper"
-    //   steps="1. Boil potatoes until soft. 2. Combine rest of ingredients to..."
-    //   />
-    // </div>
   );
 };
 
--- src/components/HomeScreen/HomeScreen.jsx
+++ src/components/HomeScreen/HomeScreen.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import NavigationBar from '../NavigationBar/NavigationBar';
 import RecipeCamera from '../CameraComponent/cameraComponent';
+import Recipe from '../Recipe/Recipe';
 
 
 function HomeScreen() {
   return (
     <div>
       <RecipeCamera />
+      <Recipe 
+        title="Samosa's"
+        imageURL="https://picsum.photos/200"
+        description="Great Indian appetizer"
+        ingredients="white potatoes, salt to taste, red pepper"
+        steps="1. Boil potatoes until soft. 2. Combine rest of ingredients to..."
+      />
       <NavigationBar />
     </div>
   );
