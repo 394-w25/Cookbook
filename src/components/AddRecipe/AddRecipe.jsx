65adc6c9e7348a0d89f1fad88cf147deb66dd390
Themanuelel <emanueldemisse2025@u.northwestern.edu>
2025-02-17 23:40:43 -0600

    use camera icon

--- src/components/AddRecipe/AddRecipe.jsx
+++ src/components/AddRecipe/AddRecipe.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom'; 
 import NavigationBar from '../NavigationBar/NavigationBar';
+import CameraAltIcon from '@mui/icons-material/CameraAlt';
 import './AddRecipe.css'; 
 
 function AddRecipe() {
   return (
     <div className="container">
-      <Link to="/cameraComponent"> 
-        <button className="button">Use Camera</button>
+      <Link to="/cameraComponent">
+        <button className="button">
+          <CameraAltIcon /> Use Camera
+        </button>
       </Link>
       <NavigationBar />
     </div>
-    
   );
 }
 
