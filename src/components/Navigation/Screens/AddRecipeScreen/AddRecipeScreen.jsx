1e93ea5ce6f6f2715b1db3d34d48e91dc431ce0e
slev1234 <150619667+slev1234@users.noreply.github.com>
2025-02-21 17:18:54 -0600

    recipe page

--- src/components/Navigation/Screens/AddRecipeScreen/AddRecipeScreen.jsx
+++ src/components/Navigation/Screens/AddRecipeScreen/AddRecipeScreen.jsx
@@ -4,17 +4,37 @@ import './AddRecipeScreen.css';
 import { useNavigate } from 'react-router-dom';
 
 function AddRecipeScreen() {
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
   return (
     <div className="container">
-      <button 
-        className="button"
-        onClick={() => navigate("/cameraComponent")} // Navigate to /cameraComponent
-      >
-        <CameraAltIcon /> Use Camera
-      </button>
-      {/* <CameraComponent/> */}
+      <h1 className="title">add recipe</h1>
+      <p className="description">How do you want to input your recipe?</p>
+
+      <div className="button-group">
+        <div className="button-container">
+          <button className="button" onClick={() => navigate("/cameraComponent")}>
+            Use a photo
+          </button>
+        </div>
+      </div>
+
+      <nav className="bottom-nav">
+        <div className="nav-container">
+          <button className="nav-button" onClick={() => navigate("/home")}>
+            Home
+          </button>
+          <button className="nav-button" onClick={() => navigate("/recipeBox")}>
+            Recipe Box
+          </button>
+          <button className="nav-button" onClick={() => navigate("/addRecipe")}>
+            Add Recipe
+          </button>
+          <button className="nav-button" onClick={() => navigate("/myFamily")}>
+            My Family
+          </button>
+        </div>
+      </nav>
     </div>
   );
 }
