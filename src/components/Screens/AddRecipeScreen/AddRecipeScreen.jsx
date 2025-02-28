59287d5da92404ee8c6134044e108e7ec316500f
Ben Cole <bctwok13@gmail.com>
2025-02-28 04:26:30 -0600

    Add recipe button styling

--- src/components/Screens/AddRecipeScreen/AddRecipeScreen.jsx
+++ src/components/Screens/AddRecipeScreen/AddRecipeScreen.jsx
@@ -1,17 +1,21 @@
 import React from 'react';
-import CameraAltIcon from '@mui/icons-material/CameraAlt';
-import './AddRecipeScreen.css'; 
 import { useNavigate } from 'react-router-dom';
+import './AddRecipeScreen.css';
+import scanImage from '@/assets/images/scanpic.png'; // Using your uploaded image
 
 function AddRecipeScreen() {
   const navigate = useNavigate();
 
   return (
     <div className="recipescreen-container">
-      <h1>How do you want to input your recipe?</h1>
-      <button className="button" onClick={() => navigate("/CameraComponent")}>
-        Use a photo {<CameraAltIcon></CameraAltIcon>}
-      </button>
+      <div className="card" onClick={() => navigate("/CameraComponent")}>
+        <div className="card-left">
+          <img src={scanImage} alt="Scan Image" className="card-image" />
+        </div>
+        <div className="card-right">
+          <span className="card-text">Scan, Upload Photo/Video</span>
+        </div>
+      </div>
     </div>
   );
 }
