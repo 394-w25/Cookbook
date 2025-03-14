4c4f05eeb247eb8dd716b01e99ac35a8bbdc5141
lestertai8 <lestertai2024@u.northwestern.edu>
2025-03-13 20:25:30 -0500

    disabled my family button

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -5,6 +5,7 @@ import RestaurantIcon from '@mui/icons-material/Restaurant';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import Diversity1 from '@mui/icons-material/Diversity1';
 import { useNavigate, useLocation } from 'react-router-dom';
+import Tooltip from '@mui/material/Tooltip';
 
 function NavigationBar() {
   const navigate = useNavigate();
@@ -18,7 +19,11 @@ function NavigationBar() {
 
   return (
     <BottomNavigation className="bottom-nav" value={value} showLabels onChange={handleChange}>
-        <BottomNavigationAction label="my family" value="my family" icon={<Diversity1 />}/>
+        <BottomNavigationAction label="my family" value="my family" icon={<Diversity1 />}
+        sx={{
+          pointerEvents: 'none',
+        }}
+        />
         <BottomNavigationAction label="add recipe" value="add recipe" icon={<AddCircleOutlineIcon />} onClick={() => navigate("/AddRecipe")} />
         <BottomNavigationAction label="recipe box" value="recipe box" icon={<RestaurantIcon />} onClick={() => navigate("/home")}/>
     </BottomNavigation>
