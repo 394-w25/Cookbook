9b9e8a176c0cdb024559ec988790b5fe06056b59
Anna Cai <annacai44@gmail.com>
2025-03-13 19:53:29 -0500

    update navbar

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -1,24 +1,26 @@
-import React, {SyntheticEvent} from 'react';
+import React, {useState} from 'react';
 import "./NavigationBar.css";
 import { BottomNavigation, BottomNavigationAction } from "@mui/material";
 import RestaurantIcon from '@mui/icons-material/Restaurant';
 import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
 import Diversity1 from '@mui/icons-material/Diversity1';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function NavigationBar() {
   const navigate = useNavigate();
-  const [value, setValue] = React.useState("home");
+  const [value, setValue] = useState("recipe box");
 
   const handleChange = (_, newValue) => {
-    setValue(newValue);
+    if (newValue !== "my family") {
+      setValue(newValue);
+    }
   };
 
   return (
     <BottomNavigation className="bottom-nav" value={value} showLabels onChange={handleChange}>
         <BottomNavigationAction label="my family" value="my family" icon={<Diversity1 />}/>
         <BottomNavigationAction label="add recipe" value="add recipe" icon={<AddCircleOutlineIcon />} onClick={() => navigate("/AddRecipe")} />
-        <BottomNavigationAction label="recipe box" value="home" icon={<RestaurantIcon />} onClick={() => navigate("/home")}/>
+        <BottomNavigationAction label="recipe box" value="recipe box" icon={<RestaurantIcon />} onClick={() => navigate("/home")}/>
     </BottomNavigation>
   );
 };
